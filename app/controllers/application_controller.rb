503abe7f0aac210e072ae2537018a4472b3b7a43
Ramon3162 <ramonomondi@gmail.com>
2020-01-06 11:31:43 +0300

    Create and login user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,30 @@
 class ApplicationController < ActionController::API
+  require 'json_web_token'
+
+  before_action :authenticate_request!
+
+  def authenticate_request!
+    if !payload || !JsonWebToken.validate_payload(payload.first)
+      return invalid_authentication
+    end
+
+    load_current_user!
+    invalid_authentication unless @current_user
+  end
+
+  def invalid_authentication
+    render json: { error: 'Invalid request' }, status: :unauthorized
+  end
+
+  def payload
+    auth_header = request.headers['Authorization']
+    token = auth_header.split(' ').last
+    JsonWebToken.decode(token)
+  rescue StandardError
+    nil
+  end
+
+  def load_current_user!
+    @current_user = User.find_by(id: payload[0]['user_id'])
+  end
 end
---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,33 @@
+class UsersController < ApplicationController
+  skip_before_action :authenticate_request!, only: %i[create login]
+
+  def create
+    user = User.new(user_params.except(:confirm_password))
+
+    user_params = params[:user]
+    if user_params[:password] != user_params[:confirm_password]
+      render json: { error: 'Passwords don\'t match' }, status: :bad_request
+    elsif user.save
+      render json: { message: 'User created successfully' }, status: :created
+    else
+      render json: { errors: user.errors.full_messages }, status: :bad_request
+    end
+  end
+
+  def login
+    user = User.find_by(email: user_params[:email].to_s.downcase)
+
+    if user&.authenticate(params[:password])
+      token = JsonWebToken.encode(user_id: user.id)
+      render json: { token: token }, status: :ok
+    else
+      render json: { error: 'Invalid email/ password' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :confirm_password)
+  end
+end
